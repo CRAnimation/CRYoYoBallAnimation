f4365db723092ba1c5f8a209f469d0c5cf4c2c62
BearRan <648070256@qq.com>
2017-05-17 23:32:03 +0800

    Initial Commit

---
+++ CRYoYoBallAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CRYoYoBallAnimation
+//
+//  Created by Bear on 2017/5/17.
+//  Copyright © 2017年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CRYoYoBallAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CRYoYoBallAnimation
+//
+//  Created by Bear on 2017/5/17.
+//  Copyright © 2017年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
